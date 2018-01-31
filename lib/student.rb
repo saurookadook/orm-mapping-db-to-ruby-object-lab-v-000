03925e5830d8bace8ee6ec25cc088fc085239331
Andy Maskiell <maskiella@gmail.com>
2018-01-31 11:03:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,9 +66,13 @@ class Student
   end
 
   def self.first_student_in_grade_10
+
+    DB[:conn].execute(sql)
   end
 
   def self.all_students_in_grade_X
+
+    DB[:conn].execute(sql)
   end
 
   def save
