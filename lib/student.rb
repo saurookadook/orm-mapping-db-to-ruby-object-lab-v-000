45f3f64308dba1472c693684d07bb9c9c63928d8
Andy Maskiell <maskiella@gmail.com>
2018-01-31 10:58:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
 
   def self.count_all_students_in_grade_9
     sql = <<-SQL
-      SELECT COUNT(students)
+      SELECT COUNT(name)
       FROM students
       WHERE grade = 9
     SQL
