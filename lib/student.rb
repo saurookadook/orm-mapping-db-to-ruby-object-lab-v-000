39e3d05db1763a061e23ad412fc6ec31cc7c8bf9
Andy Maskiell <maskiella@gmail.com>
2018-01-31 11:03:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,6 +55,14 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(value)
+    sql = <<-SQL
+      SELECT name
+      FROM students
+      WHERE grade = 10
+      LIMIT 2
+    SQL
+
+    DB[:conn].execute(sql)
   end
 
   def self.first_student_in_grade_10
