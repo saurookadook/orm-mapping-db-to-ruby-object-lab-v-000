5286c6457eda3c4017f04d0a11b24b8a3efdc8e1
Andy Maskiell <maskiella@gmail.com>
2018-01-31 11:10:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,9 +75,7 @@ class Student
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+    DB[:conn].execute(sql)
   end
 
   def self.all_students_in_grade_X(value)
