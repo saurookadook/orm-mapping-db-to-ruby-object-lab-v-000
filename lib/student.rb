da58a8e7c1228563573fe0bae684ed500690d37f
Andy Maskiell <maskiella@gmail.com>
2018-01-31 11:01:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,7 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.first_X_students_in_grade_10(int)
+  def self.first_X_students_in_grade_10(amount)
   end
 
   def self.first_student_in_grade_10
